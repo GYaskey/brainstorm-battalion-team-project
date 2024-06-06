b589728e7b3ac3823ea3b1535bfc7c7ebe744dba
Gleb Yasynskyi <gleb.yasinsky@gmail.com>
2024-06-06 16:38:04 +0300

    add header menu js

---
+++ src/main.js
@@ -0,0 +1,10 @@
+const backdrop = document.getElementById('backdrop');
+const openButton = document.getElementById('openButton');
+const closeButton = document.getElementById('closeButton');
+
+openButton.addEventListener('click', () => {
+    backdrop.classList.add('is-open');
+});
+
+closeButton.addEventListener('click', () => {
+    backdrop.classList.remove('is-open');
